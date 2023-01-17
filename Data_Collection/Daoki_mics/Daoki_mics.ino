2696321cbb6ccfc4b045995a5367f057344a329d
ReDay Zarra <redayzarra@gmail.com>
2023-01-16 22:12:45 -0500

    Finishing code on collecting Daoki mic data

---
+++ Data_Collection/Daoki_mics/Daoki_mics.ino
@@ -0,0 +1,42 @@
+#include <SD.h> // Include the SD library
+const int MIC_PIN = A0; // Connect the microphone to analog pin A0
+const int chipSelect = 10; // Chip select pin for the SD card
+File dataFile; // Create a File object
+
+// SD card must be formatted as FAT16 or FAT32 and must be connected to the chipSelect pin and the 3.3V pin. 
+// Also make sure that the library is correctly installed and the board is compatible with SD card module.
+
+void setup() {
+
+  Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+
+  // Initialize the SD card
+  pinMode(chipSelect, OUTPUT);
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD card initialization failed");
+    return;
+  }
+
+  // Create a new file on the SD card
+  dataFile = SD.open("mic_data.csv", FILE_WRITE);
+  if (!dataFile) {
+    Serial.println("Error creating file");
+    return;
+  }
+}
+
+void loop() {
+
+  int micValue = analogRead(MIC_PIN); // Read the value from the microphone
+
+  Serial.println(micValue); // Send the value to the serial monitor
+  
+  // Write the value to the .csv file
+  dataFile.print(micValue);
+  dataFile.print(",");
+  delay(100); // Wait for 100 milliseconds
+}
+
+void exit() {
+  dataFile.close(); // Close the file when the program exits
+}
--- README.md
+++ README.md
@@ -16,7 +16,7 @@ The objective is to detect changes in the functional status of homebound patient
 
 **Movement detection:** ESP32 bluetooth transmitters to detect a wearable device on the target patient. When the device is in range, the system can initialize MLX90640 thermal cameras (unsure) and the central data collection system.
 
-**Sleep detection:** A central data collection hub, powered by an ELEGOO UNO R3 board and several environmental sensors, will initialize to gather as much data as possible. The system collects data regularly when patient is not detected, but is always active when patient is in range. Embedded machine learning models, using TinyML technology, on the microcontroller analyze the data and return results to a server.
+**Sleep detection:** A central data collection hub, powered by an ELEGOO UNO R3 board and several environmental sensors, will initialize to gather as much data as possible. The system collects data regularly when the patient is not detected but is always active when the patient is in range. Embedded machine learning models, using TinyML technology, on the microcontroller analyze the data and return results to a server.
  
 <div align="center">
 
