5ddc22622f287dea319ff0b5f632a4ca56c9d928
ReDay Zarra <redayzarra@gmail.com>
2023-02-25 23:17:14 -0500

    Adding debugging for SD card

--- Machine_Learning/Marvin Model/CollectData/CollectData.ino
+++ Machine_Learning/Marvin Model/CollectData/CollectData.ino
@@ -23,3 +23,18 @@ void setup() {
     Serial.println("Failed to initialize LPS sensor!"); // Give a warning if LPS Sensor fails
     while (1);
   }
+
+  if (!APDS9960.begin()) {
+    Serial.println("Failed to initialize APDS sensor!"); // Give a warning if APDS Sensor fails
+    while (1);
+  }
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize LSM sensor!"); // Give a warning if LSM Sensor fails
+    while (1);
+  }
+
+  if (!SD.begin(4)) {
+    Serial.println("Failed to initialize SD card!"); // Give a warning if SD Card fails
+    while (1);
+  }
