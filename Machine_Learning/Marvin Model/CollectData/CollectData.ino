d9f7727189f27e5e1d6e9b9d41e38656dab540cd
ReDay Zarra <redayzarra@gmail.com>
2023-02-27 22:48:23 -0500

    Updating data collection file

--- Machine_Learning/Marvin Model/CollectData/CollectData.ino
+++ Machine_Learning/Marvin Model/CollectData/CollectData.ino
@@ -9,39 +9,4 @@
 
 File dataFile;
 
-void setup() {
 
-  Serial.begin(9600);
-  while (!Serial) {}
-
-  if (!HTS.begin()) {
-    Serial.println("Failed to initialize HTS sensor!"); // Give a warning if HTS Sensor fails
-    while (1);
-  }
-
-  if (!LPS22HB.begin()) {
-    Serial.println("Failed to initialize LPS sensor!"); // Give a warning if LPS Sensor fails
-    while (1);
-  }
-
-  if (!APDS9960.begin()) {
-    Serial.println("Failed to initialize APDS sensor!"); // Give a warning if APDS Sensor fails
-    while (1);
-  }
-
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize LSM sensor!"); // Give a warning if LSM Sensor fails
-    while (1);
-  }
-
-  if (!SD.begin(4)) {
-    Serial.println("Failed to initialize SD card!"); // Give a warning if SD Card fails
-    while (1);
-  }
-
-  dataFile = SD.open("data.csv", FILE_WRITE);
-
-  if (!dataFile) {
-    Serial.println("Failed to open data.csv file!"); // Give a warning if csv file can't open
-    while (1);
-}
