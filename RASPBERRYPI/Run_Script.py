9e755e2d04b4964dda51667b7cb87f961907830a
ReDay Zarra <redayzarra@gmail.com>
2023-04-29 07:39:41 -0400

    Adding code for breath calculation and MongoDB configuration

--- RASPBERRYPI/Run_Script.py
+++ RASPBERRYPI/Run_Script.py
@@ -7,9 +7,9 @@ import pymongo
 from pymongo import MongoClient
 import time
 import numpy as np
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, lfilter, find_peaks
 
-# Bandpass filter functions
+# AUDIO PROCESSING - Bandpass filter functions: These filters are trying to only keep breathing noises.
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
     low = lowcut / nyq
@@ -23,7 +23,7 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     return y
 
 def process_audio_for_breathing(audio_data, sample_rate=16000):
-    # Define the breathing frequency range (0.1 to 0.5 Hz)
+    # Define the breathing frequency range (0.1 to 0.5 Hz) - Adjust this value based on your own data. These are generic values based on my research.
     lowcut = 0.1
     highcut = 0.5
 
@@ -31,3 +31,30 @@ def process_audio_for_breathing(audio_data, sample_rate=16000):
     filtered_signal = butter_bandpass_filter(audio_data, lowcut, highcut, sample_rate)
 
     return filtered_signal
+
+# CALCULATING BREATHS PER MINUTE - Using the find_peaks algorithm
+def calculate_breaths_per_minute(filtered_signal, sample_rate, min_peak_height=0.01, min_peak_distance=0.5):
+    # Detect peaks in the filtered signal
+    min_distance = int(sample_rate * min_peak_distance)
+    peaks, _ = find_peaks(filtered_signal, height=min_peak_height, distance=min_distance)
+
+    # Calculate breaths per minute
+    if len(peaks) > 1:
+        breath_duration = np.mean(np.diff(peaks)) / sample_rate
+        breaths_per_minute = 60 / breath_duration
+    else:
+        breaths_per_minute = 0
+
+    return breaths_per_minute
+
+# MongoDB configuration - The following are credentials to connect to MongoDB
+MONGODB_CONNECTION_STRING = 'mongodb+srv://<username>:<password>@<cluster_url>/test?retryWrites=true&w=majority'
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client['your_database_name'] # This is the database name
+collection = db['your_collection_name'] # This is the collection name
+
+ser = serial.Serial('/dev/ttyACM0', 9600)  # You may need to adjust the port depending on your setup
+
+audio_buffer = []
+buffer_size = 16000  # Adjust this value based on the desired buffer size. This processes one second of audio at a time (assuming a sample rate of 16,000 Hz).
+
